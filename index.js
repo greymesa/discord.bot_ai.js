f9baea1eaa20d4fc945aedfe476924ccac6a2d76
Magnitopic <magnitopic@gmail.com>
2021-09-08 19:08:50 +0200

    fixed hello there bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client( { disableMentions: "all" } );
+const client = new Discord.Client({ disableMentions: "all" });
 require('dotenv').config();
 
 client.commands = new Discord.Collection();
@@ -50,69 +50,68 @@ client.on('messageCreate', (msg) => {
     }
 });
 //habla
-client.on('message',msg=>{
-    switch(msg.content.toLowerCase()){
+client.on('message', msg => {
+    switch (msg.content.toLowerCase()) {
         case "presio":
-            msg.reply('muycaro gasias') 
-        break;
+            msg.reply('muycaro gasias')
+            break;
         case "puta":
-            msg.reply('mezcla pures') 
-        break;
+            msg.reply('mezcla pures')
+            break;
         case "hello there":
-            msg.send('https://c.tenor.com/QFSdaXEwtBAAAAAC/hello-there-general-kenobi.gif')
+            msg.replay('https://c.tenor.com/QFSdaXEwtBAAAAAC/hello-there-general-kenobi.gif')
             break;
         case "roblox":
-            msg.reply('vete al minecraft') 
-        break;         
+            msg.reply('vete al minecraft')
+            break;
         case "xd":
-            msg.reply('... ||i have 2 say it||||xd´nt||') 
-        break;        
+            msg.reply('... ||i have 2 say it||||xd´nt||')
+            break;
         case "uwu":
-            msg.reply('shootgun') 
-        break;        
+            msg.reply('shootgun')
+            break;
         case "jotaro":
-            msg.reply('DIO') 
-        break;        
+            msg.reply('DIO')
+            break;
         case "sad":
-            msg.reply(':(') 
-        break;        
+            msg.reply(':(')
+            break;
         case "trifusion":
-            msg.reply('masca chapas') 
-        break;       
-         case "let me in":
-        msg.sendMessage('https://images-ext-2.discordapp.net/external/7fNdnVEIzJmSvtDFC2lwyce3cFSD20FgwxQ2zQquT90/%3Fcid%3D73b8f7b11e3dde6c0645f54cc08979d3636464c9d2aa4594%26rid%3Dgiphy.mp4%26ct%3Dg/https/media2.giphy.com/media/yx400dIdkwWdsCgWYp/giphy.mp4') 
-        break; 
+            msg.reply('masca chapas')
+            break;
+        case "let me in":
+            msg.sendMessage('https://images-ext-2.discordapp.net/external/7fNdnVEIzJmSvtDFC2lwyce3cFSD20FgwxQ2zQquT90/%3Fcid%3D73b8f7b11e3dde6c0645f54cc08979d3636464c9d2aa4594%26rid%3Dgiphy.mp4%26ct%3Dg/https/media2.giphy.com/media/yx400dIdkwWdsCgWYp/giphy.mp4')
+            break;
         case "69":
-            msg.reply('Nice') 
-        break;        
+            msg.reply('Nice')
+            break;
         case "thicc":
-            msg.reply('ur mom') 
-        
-        break;       
+            msg.reply('ur mom')
+            break;
         case "me too kid":
-            msg.reply('https://i.imgflip.com/433z6b.png') 
-        break;        
+            msg.reply('https://i.imgflip.com/433z6b.png')
+            break;
         case "never gonna give you up":
-            msg.reply('never let you down') 
-        break;       
+            msg.reply('never let you down')
+            break;
         case "kill me":
-            msg.reply('https://i.pinimg.com/originals/f0/03/29/f00329c74ed15d7696be7ed87a1e2691.gif') 
-        break;
+            msg.reply('https://i.pinimg.com/originals/f0/03/29/f00329c74ed15d7696be7ed87a1e2691.gif')
+            break;
         case "feed my coin":
-            msg.reply('https://youtu.be/6RU5sVP03fY') 
-        break;
+            msg.reply('https://youtu.be/6RU5sVP03fY')
+            break;
         case "i like your funny words magic man":
-        msg.reply('https://i.kym-cdn.com/photos/images/newsfeed/001/909/636/a8c.png') 
-        break;
-        break;
+            msg.reply('https://i.kym-cdn.com/photos/images/newsfeed/001/909/636/a8c.png')
+            break;
+            break;
         case "shut up and take a money":
-        msg.reply('https://i.pinimg.com/originals/86/e6/2f/86e62f136d93bc6bffada430bed0afed.jpg') 
-        break;
+            msg.reply('https://i.pinimg.com/originals/86/e6/2f/86e62f136d93bc6bffada430bed0afed.jpg')
+            break;
 
-        break;
+            break;
         case "hl3":
-        msg.reply('https://images-ext-2.discordapp.net/external/PcEowWZQLuGd0v6ATKO805XLKzb9Iz-lsg_sUqZgTFE/%3Fauto%3Dwebp%26s%3D7ed0d4cb43c6a6dc80a6e116ffce7a6f24aca89a/https/preview.redd.it/ewfgydv9jwn01.png haha no') 
-        break; 
+            msg.reply('https://images-ext-2.discordapp.net/external/PcEowWZQLuGd0v6ATKO805XLKzb9Iz-lsg_sUqZgTFE/%3Fauto%3Dwebp%26s%3D7ed0d4cb43c6a6dc80a6e116ffce7a6f24aca89a/https/preview.redd.it/ewfgydv9jwn01.png haha no')
+            break;
 
     }
 })
@@ -120,32 +119,32 @@ client.on('message',msg=>{
 //no funsiona
 
 //con prefix
-client.on('message',message=>{
+client.on('message', message => {
 
-    let args =message.content.substring(PREFIX.length).split(" ");
+    let args = message.content.substring(PREFIX.length).split(" ");
 
-    switch(args[0]){
+    switch (args[0]) {
         case 'getGF':
             message.channel.send('got to tinder you horny!');
-        break;  
+            break;
         case 'Kill':
             message.channel.send('okay!:gun:');
-        break;
+            break;
         case 'help':
             message.channel.send('ping me and write something to use the AI,use !roll to play 8ball');
-        break;  
+            break;
         case 'do':
-      
+
     }
 })
 //ping
 client.on('message', message => {
-    if (message.content === '!ping') {  
-      message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-      //message.channel.send('also pong!')
+    if (message.content === '!ping') {
+        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        //message.channel.send('also pong!')
     }
-  });
-  
+});
+
 //fin-ping
 //fin-habla
 //8ball
@@ -157,19 +156,19 @@ const clockEmoji = ["🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "�
 
 const re = /^(\!\s?r+o+ll+|\!\s?s+h+a+k+e+)(?:\s(.+))?/gim; // Regex to find out if a user types ">roll"
 let responses;
-fs.readFile('./responses8ball.txt', "utf8", function(err, data) { // Allows custom responses
-    if(err) {
+fs.readFile('./responses8ball.txt', "utf8", function (err, data) { // Allows custom responses
+    if (err) {
         console.log("Can't read or find responses.txt, setting defaults");
         responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
-                    "You may rely on it", "As I see it, yes", "Most Likely", "Outlook good", "Yes",
-                    "Signs point to yes", "Reply hazy try again", "Ask again later"];
+            "You may rely on it", "As I see it, yes", "Most Likely", "Outlook good", "Yes",
+            "Signs point to yes", "Reply hazy try again", "Ask again later"];
     } else {
         responses = data.split("\n"); // Responses are each individual line
     }
 });
 
 client.on('ready', () => {
-      console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
     console.log("Available responses: " + responses.length);
     //client.user.setGame(">roll or >shake");
 });
@@ -177,21 +176,21 @@ client.on('ready', () => {
 client.on('message', msg => {
     let matches = re.exec(msg.content);
 
-      if (!msg.author.bot && matches) {
-          if(limit && limit.exists(msg.author.id)) { // Checks if a usage limit is in place
-              for(let i = 0; i < Math.floor(Math.random()*5); i++) {
-                msg.react(clockEmoji[Math.floor(Math.random()*clockEmoji.length)]);
-              }
+    if (!msg.author.bot && matches) {
+        if (limit && limit.exists(msg.author.id)) { // Checks if a usage limit is in place
+            for (let i = 0; i < Math.floor(Math.random() * 5); i++) {
+                msg.react(clockEmoji[Math.floor(Math.random() * clockEmoji.length)]);
+            }
         } else {
-            var rand = Math.floor(Math.random()*responses.length);
-              if(matches[2]) {
-                  msg.reply("You asked: \"" + matches[2] + "\" - " + responses[rand]);
-              } else {
-                  msg.reply(responses[rand]);
-              }
-              limit.add(msg.author.id);
+            var rand = Math.floor(Math.random() * responses.length);
+            if (matches[2]) {
+                msg.reply("You asked: \"" + matches[2] + "\" - " + responses[rand]);
+            } else {
+                msg.reply(responses[rand]);
+            }
+            limit.add(msg.author.id);
         }
-      }
+    }
 });
 
 
